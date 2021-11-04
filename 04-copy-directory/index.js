00d408a34d4c7253e1de7f750a23b6a1c2dbadfb
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2021-11-04 17:19:48 +0300

    feat: complete o4-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
 
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const originDirPath = path.join(__dirname, 'files');
+const copyDirPath = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  try {
+    await fsPromises.access(copyDirPath, fs.constants.W_OK);
+    const copyDirFiles = await fsPromises.readdir(copyDirPath);
+    copyDirFiles.forEach(file => {
+      const filePath = path.join(copyDirPath, file);
+      fsPromises.unlink(filePath);
+    });
+  } catch {
+    await fsPromises.mkdir(copyDirPath, { recursive: true });
+  }
+
+  const files = await fsPromises.readdir(originDirPath);
+  files.forEach(async (file) => {
+    const originFilePath = path.join(__dirname, 'files', file);
+    const copyFilePath = path.join(__dirname, 'files-copy', file);
+    await fsPromises.copyFile(originFilePath, copyFilePath);
+  });
+}
+
+copyDir();
